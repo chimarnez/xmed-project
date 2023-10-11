a1d62fe9b0369d33594b752c65a75c99bbe19d98
chimarnez <chimarnez@gmail.com>
2023-10-11 17:17:51 -0600

    Create patient service

---
+++ src/services/patient.js
@@ -0,0 +1,22 @@
+const Patient = require("../models/patient");
+
+exports.findAll = function () {
+  return Patient.findAll();
+};
+
+exports.findById = function (id) {
+  return Patient.findByPk(id);
+};
+
+exports.insert = function (data) {
+  return Patient.create(data);
+};
+
+exports.update = async function (id, data) {
+  await Patient.update(data, { where: id });
+};
+
+exports.deleteById = async function (id) {
+  const patient = await Patient.findByPk(id);
+  await patient.destroy();
+};
