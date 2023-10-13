7c6979c3fa4b817d78b0992dec5f078b6206d618
Yasser Rosas <yassros16@gmail.com>
2023-10-13 16:40:36 -0600

    Documentation initialized with schemas

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 require('dotenv').config()
 const express = require('express')
+const swaggerUi = require('swagger-ui-express')
 const app = express()
 
 const { initDatabase } = require('./db')
+const openApiConfig = require('./docs/swagger')
 initDatabase()
 
 app.use(express.json())
+app.use('/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(openApiConfig))
 
 const userRouter = require('./routers/user')
 const authRouter = require('./routers/auth')
