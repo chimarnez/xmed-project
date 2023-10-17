9026732f9f28936279bf2cf081a284c8c1b47e7d
Yasser Rosas <yassros16@gmail.com>
2023-10-17 12:29:54 -0600

    Record finished and sequelize model changed to work with mysql

--- src/controllers/record.js
+++ src/controllers/record.js
@@ -1,4 +1,4 @@
-const { save, findById, updateById, deleteById } = require('../services/record')
+const { save, findById, updateById, deleteById, findByPatientId, findByDoctorId } = require('../services/record')
 
 exports.createRecord = async function (req, res) {
   const { id: DoctorId } = req.user.Doctor
@@ -22,10 +22,28 @@ exports.createRecord = async function (req, res) {
 //   return res.status(200).json(records)
 // }
 
-exports.getRecord = async function (req, res) {
+exports.getRecordById = async function (req, res) {
   const { id } = req.params
   const record = await findById(id)
   return res.status(200).json(record)
+}
+
+exports.getRecordsFromDoctorByPatientId = async function (req, res) {
+  const { id: PatientId } = req.params
+  const records = await findByPatientId(PatientId)
+  return res.status(200).json(records)
+}
+
+exports.getRecordsByPatientId = async function (req, res) {
+  const { id: PatientId } = req.user.Patient
+  const records = await findByPatientId(PatientId)
+  return res.status(200).json(records)
+}
+
+exports.getRecordByDoctorId = async function (req, res) {
+  const { id: DoctorId } = req.user.Doctor
+  const records = await findByDoctorId(DoctorId)
+  return res.status(200).json(records)
 }
 
 exports.updateRecord = async function (req, res) {
--- src/routers/record.js
+++ src/routers/record.js
@@ -1,28 +1,37 @@
 const express = require('express')
 const router = express.Router()
 const validator = require('../middlewares/validator')
-const { jwtValidator, jwtValidatorDoctor } = require('../middlewares/jwt')
+const { jwtValidator, jwtValidatorDoctor, jwtValidatorPatient } = require('../middlewares/jwt')
 const {
-  createRecordSchema,
-  updateRecordSchema
+  createRecordSchema
 } = require('../validations/record')
 const {
   createRecord,
-  getRecord,
-  updateRecord,
-  deleteRecord
+  getRecordById,
+  getRecordsByPatientId,
+  deleteRecord,
+  getRecordByDoctorId,
+  getRecordsFromDoctorByPatientId
 } = require('../controllers/record')
 
 router.post('/', jwtValidatorDoctor, validator.body(createRecordSchema), createRecord)
 
-router.get('/:id', jwtValidator, getRecord)
+router.get('/patients', jwtValidatorPatient, getRecordsByPatientId)
 
-router.put(
-  '/:id',
-  jwtValidator,
+router.get('/patients/:id', jwtValidatorPatient, getRecordById)
+
+router.get('/doctors', jwtValidatorDoctor, getRecordByDoctorId)
+
+router.get('/doctors/patient/:id', jwtValidatorDoctor, getRecordsFromDoctorByPatientId)
+
+router.get('/doctors/:id', jwtValidatorDoctor, getRecordById)
+
+/* router.put(
+  '/doctors/:id',
+  jwtValidatorDoctor,
   validator.body(updateRecordSchema),
   updateRecord
-)
+) */
 
 router.delete('/:id', jwtValidator, deleteRecord)
 
