fac112277bc366df74c745a987e3acf32d7de1aa
Yasser Rosas <yassros16@gmail.com>
2023-12-05 01:22:24 -0600

    update profile-picture added

--- src/routers/user.js
+++ src/routers/user.js
@@ -7,12 +7,14 @@ const {
   getUsers,
   updateUser,
   deleteUser,
-  getUserRole
+  getUserRole,
+  uploadUserProfilePicture
 } = require('../controllers/user')
 const validator = require('../middlewares/validator')
 const { jwtValidator } = require('../middlewares/jwt')
 const { createUserSchema, updateUserSchema } = require('../validations/user')
 const { withUser } = require('../middlewares/user')
+const upload = require('../middlewares/files')
 
 router.post('/', withUser, validator.body(createUserSchema), createUser)
 router.get('/profile', jwtValidator, getUser)
@@ -20,6 +22,7 @@ router.get('/', jwtValidator, getUserById)
 router.get('/', jwtValidator, getUsers)
 router.patch('/', jwtValidator, validator.body(updateUserSchema), updateUser)
 router.delete('/', jwtValidator, deleteUser)
+router.post('/profile-picture', jwtValidator, upload.single('profilePicture'), uploadUserProfilePicture)
 
 router.get('/role', jwtValidator, getUserRole)
 
